956fc510aa6762d313b1a08d74910945693a7e77
DevLost - Laptop <gabi_tlc@yahoo.com>
2022-09-22 23:52:12 +0300

    Added DataTable and Filter Select

---
+++ wwwroot/js/table.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    // Setup - add a text input to each footer cell
+    $(document).ready(function () {
+        $('#example').DataTable({
+            initComplete: function () {
+                var i=0;
+                this.api()
+                    .columns()
+                    .every(function () {
+                        var column = this;
+                        var select = i===0 ? $('<select style="opacity: 0; pointer-events: none"><option value=""></option></select>')
+                            .appendTo($(column.footer()).empty())
+                            .on('change', function () {
+                                var val = $.fn.dataTable.util.escapeRegex($(this).val());
+
+                                column.search(val ? '^' + val + '$' : '', true, false).draw();
+                            }) : $('<select><option value=""></option></select>')
+                            .appendTo($(column.footer()).empty())
+                            .on('change', function () {
+                                var val = $.fn.dataTable.util.escapeRegex($(this).val());
+
+                                column.search(val ? '^' + val + '$' : '', true, false).draw();
+                            });
+
+                        column
+                            .data()
+                            .unique()
+                            .sort()
+                            .each(function (d, j) {
+                                select.append('<option value="' + d + '">' + d + '</option>');
+                            });
+                        i++;
+                    });
+            },
+        });
+    });
+});
