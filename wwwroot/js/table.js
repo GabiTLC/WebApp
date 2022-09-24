9422f2586e7dbf52984bb5fc9ab6c47ab79e838b
DevLost - Laptop <gabi_tlc@yahoo.com>
2022-09-24 16:20:54 +0300

    Table layout completed

--- wwwroot/js/table.js
+++ wwwroot/js/table.js
@@ -2,22 +2,24 @@ $(document).ready(function () {
     // Setup - add a text input to each footer cell
     $(document).ready(function () {
         $('#example').DataTable({
-            dom: "<'position-relative'<'position-absolute pb-5 top-0 start-0'f>>",
-                
+            pagingType: 'full_numbers',
+            responsive: true,
+            dom: "<'float-left pt-5' f>" + "<'float-right pt-5' l>" + "<'d-flex justify-content-center pr-5 pb-5 'p>" + "<t>" + 
+                "<'float-left' i>" + "<'float-right mt-2' l>" + "<'d-flex justify-content-center mt-5 pl-5 'p>",               
             initComplete: function () {
                 var i=0;
                 this.api()
                     .columns()
                     .every(function () {
                         var column = this;
-                        var select = i===0 ? $('<select style="opacity: 0; pointer-events: none"><option value=""></option></select>')
-                            .appendTo($(column.footer()).empty())
+                        var select = i===0 || i===1 ? $('<select style="opacity: 0; pointer-events: none"><option value=""></option></select>')
+                            .appendTo($(column.header()).empty())
                             .on('change', function () {
                                 var val = $.fn.dataTable.util.escapeRegex($(this).val());
 
                                 column.search(val ? '^' + val + '$' : '', true, false).draw();
                             }) : $('<select><option value=""></option></select>')
-                            .appendTo($(column.footer()).empty())
+                            .appendTo($(column.header()).empty())
                             .on('change', function () {
                                 var val = $.fn.dataTable.util.escapeRegex($(this).val());
 
