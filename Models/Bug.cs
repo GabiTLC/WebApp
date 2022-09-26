f4a0be1a1ba4f86003e902c4ae536fae508491ed
DevLost - Laptop <gabi_tlc@yahoo.com>
2022-09-26 18:43:00 +0300

    New migratins v2

--- Models/Bug.cs
+++ Models/Bug.cs
@@ -8,14 +8,21 @@ public class Bug
     public int Id { get; set; }
     [Required]
     public string Summary { get; set; }
+    [Required]
     public string Type { get; set; } //dropdown
     [Required]
-    public string Priority { get; set; }   //dropdown
+    public string Priority { get; set; } //dropdown
     public char Severity { get; set; }   //dropdown
     public string Platform { get; set; }    //dropdown
+    [Required]
+    [MaxLength(5000)]
     public string Description { get; set; } //areatext
-    public string Attachment { get; set; }
-    public string StepsToRepo { get; set; }     //areatext
+    public string StepsToRepo { get; set; }
+    
+    [Required]
+    [MaxLength(5000)]
     public string ExpectedResult { get; set; }  //area text
+    
+    public string Attachment { get; set; }
     
 }
