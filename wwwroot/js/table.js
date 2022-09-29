2553d3f7cf5694e08a2c9ce7ec1c94288b9c1384
DevLost - Laptop <gabi_tlc@yahoo.com>
2022-09-30 00:38:10 +0300

    Added Pie Charts to the table page

--- wwwroot/js/table.js
+++ wwwroot/js/table.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
      let id=0;
-     $('#example').DataTable({
+     let table = $('#example').DataTable({
             pagingType: 'full_numbers',         // first, previous, next and last buttons on the table
             responsive: true,  //makes the table responsive
             orderCellsTop: true,
@@ -63,7 +63,133 @@ $(document).ready(function () {
                     });
                 
            },
-    });
      
+     });
+     Highcharts.setOptions({
+        colors: Highcharts.map(Highcharts.getOptions().colors, function (color) {
+            return {
+                radialGradient: {
+                    cx: 0.5,
+                    cy: 0.3,
+                    r: 0.7
+                },
+                stops: [
+                    [0, color],
+                    [1, Highcharts.color(color).brighten(-0.3).get('rgb')] // darken
+                ]
+            };
+        })
+     });
+
+     var chart1 = Highcharts.chart('chart1', {
+        chart: {
+            type: 'pie',
+            plotBackgroundColor: '#bb0404',
+            plotBorderWidth: null,
+            options3d: {
+                enabled: true,
+                alpha: 45,
+                beta: 0
+            },
+        },
+        title: {
+            text: '<span style=""> Bug Count Per Type</span>',                //add style
+        },
+        tooltip: {
+            headerFormat: '',
+            pointFormat:'<span style="color:{point.color}">\u25CF</span> <b> {point.name}</b><br/>' +
+                'No. of bugs: <b>{point.y}</b><br/>' +
+                'Procentage of displayed bugs: <b>{point.percentage:.1f}%</b><br/>'
+            },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                depth: 45,
+                dataLabels: {
+                    enabled: true,
+                    connectorColor: 'silver',
+                    format: '<b>{point.name}</b>'
+                }
+            }
+        },
+        series: [
+            {
+                name: 'Bugs',
+                data: chartData(table,2),
+                showInLegend: true,
+            },
+        ],
+    });
+
+     var chart2 = Highcharts.chart('chart2', {
+        chart: {
+            type: 'pie',
+            plotBackgroundColor: '#bb0404',
+            plotBorderWidth: null,
+            options3d: {
+                enabled: true,
+                alpha: 45,
+                beta: 0
+            },
+        },
+        title: {
+            text: 'Bug Count Per Status',
+        },
+        tooltip: {
+            headerFormat: '',
+            pointFormat:'<span style="color:{point.color}">\u25CF</span> <b> {point.name}</b><br/>' +
+                'No. of bugs: <b>{point.y}</b><br/>' +
+                'Procentage of displayed bugs: <b>{point.percentage:.1f}%</b><br/>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                depth: 45,
+                dataLabels: {
+                    enabled: true,
+                    connectorColor: 'silver',
+                    format: '<b>{point.name}</b>'
+                }
+            }
+        },
+        series: [
+            {
+                name: 'Bugs',
+                data: chartData(table,7),
+                showInLegend: true,
+            },
+        ],
+    });
+    // On each draw, update the data in the chart
+    
+     table.on('draw', function () {
+        chart1.series[0].setData(chartData(table,2));
+        chart2.series[0].setData(chartData(table,7));
+    });
     
 });
+function chartData(table,column) {
+    var counts = {};
+
+    // Count the number of entries for each position
+    table
+        .column(column, { search: 'applied' })
+        .data()
+        .each(function (val) {
+            if (counts[val]) {
+                counts[val] += 1;
+            } else {
+                counts[val] = 1;
+            }
+        });
+
+    // And map it to the format highcharts uses
+    return $.map(counts, function (val, key) {
+        return {
+            name: key,
+            y: val,
+        };
+    });
+}
