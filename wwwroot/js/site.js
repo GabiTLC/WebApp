c1bc3211d2f871ccac7fde3e467d4e4ab1e24a9d
DevLost - Laptop <gabi_tlc@yahoo.com>
2022-10-05 01:53:45 +0300

    Added sections to main page

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function(){
+    var href = window.location.href;
+    var splitit = (href.split('#'))[1]; //split url to get sec1 name
+    if(splitit !== "" || splitit !== "undefined"){
+        $('html, body').animate({
+            scrollTop: $('#'+splitit).offset().top
+        }, 2000);
+    }
+});
